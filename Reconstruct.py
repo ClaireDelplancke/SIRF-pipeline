effd1c6cfc21c19f157c6ed8a87c9835b6f78934
ClaireDelplancke <cd902@bath.ac.uk>
2022-05-11 13:38:42 +0100

    Move reconstruct example script in documentation

--- Reconstruct.py
+++ Reconstruct.py
@@ -3,6 +3,16 @@ Author: Claire Delplancke, University of Bath, PET++ project
 2022
 https://clairedelplancke.github.io
 https://petpp.github.io
+
+Example usage
+python Reconstruct.py --folder_input 'folder absolute path' \
+				--folder_output 'folder absolute path'  \
+				--nepoch 200 \
+				--nsave 100 \
+				--nobj 10 \
+				--reg 'FGP-TV' \
+                --nsubsets 1 \
+				--fast
 """
 
 from calendar import day_abbr
