888b8c1dcfa1415c0abf939aa34a94f56860ac8b
ClaireDelplancke <cd902@bath.ac.uk>
2022-05-23 10:36:14 +0100

    Add example usage for registration script

--- register.sh
+++ register.sh
@@ -3,6 +3,9 @@
 # second argument is the path to the .nii PET reconstruction
 # this will create two .nii files in the DICOM folder: reoriented.nii and registered.nii
 
+# example usage
+# ./register.sh /home/cd902/data_azure/Patients/12560/scanner_mr /home/cd902/data_azure/Patients/12560/pet++_reco/full/spdhg_reg_CIL-TV_alpha3.0_warmstart1_nsub1_precond1_a1_n1_d1_r1_s1_iters_600.nii
+
 if [ $# -ne 2 ]; then
   echo "Needs two arguments"
   exit 1
